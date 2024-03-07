a2a89ddec5d6f034c4afd8a20f0ea9a3e1eef536
LeoCodigos <90196670+LeoCodigos@users.noreply.github.com>
2024-03-07 11:59:12 -0300

    ButtonsNetwork_Update

---
+++ MultiplayerFlag/Assets/Scripts/NetworkManagerUI.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Unity.Netcode;
+
+public class NetworkManagerUI : MonoBehaviour
+{
+    #region Atributos
+
+    [SerializeField] private Button buttonServer,buttonHost,buttonClient;
+
+    #endregion
+ 
+    void Awake()
+    {
+        buttonHost.onClick.AddListener(()=>
+        {
+            NetworkManager.Singleton.StartHost();
+        });
+
+        buttonClient.onClick.AddListener(()=>{
+            NetworkManager.Singleton.StartClient();
+        });
+
+        buttonServer.onClick.AddListener(()=>{
+            NetworkManager.Singleton.StartServer();
+        });
+    }
+}
